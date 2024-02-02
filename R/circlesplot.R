895c5135a973de2aefb32bc1ce8d519ef96d2e26
BenSt099 <ben.steinhauer.de@gmail.com>
2024-02-02 14:09:05 +0100

    updated functions

--- R/circlesplot.R
+++ R/circlesplot.R
@@ -1,43 +1,66 @@
 library("plotrix")
 
-#circlesplot(c(1,2,3,4), c('Earth', 'Sun','s','s'))
+#circlesplot(cp_vals=c(4,3,2,1), cp_text=c('4','3','2','1'), cp_max=4L, cp_title="Planets")
 
 
-circlesplot <- function(df_values, df_text, max=10L, line_width=3) {
 
 
-  if (class(max) != "integer") {
-    stop("[Error][circlesplot][Error in Parameter(s)]: Parameter 'max' should be integer")
-  }
-  if (length(df_values) != length(df_text)) {
-    stop("[Error][circlesplot][Error in Parameter(s)]: Both vectors should have same length")
-  }
-  if (is.numeric(df_values) != TRUE) {
-    stop("[Error][circlesplot][Error in Parameter(s)]: One vector should contain numericals")
-  }
-  if (is.character(df_text) != TRUE) {
-    stop("[Error][circlesplot][Error in Parameter(s)]: One vector should contain characters")
-  }
+circlesplot <- function(cp_vals=NULL, cp_text=NULL, cp_max=10L, cp_line_width=3L, cp_title="") {
 
-  df <- data.frame(df_values, df_text)
-  df <- df[order(df$df_values, decreasing = TRUE),]
+  .check_params(cp_vals, cp_text, cp_max, cp_line_width, cp_title)
 
-  plot(0, 0, type = "n", xlim = c(-9, 9), ylim = c(-9, 9), axes=TRUE, asp=1)
+  df <- data.frame(cp_vals, cp_text)
+  df <- df[order(df$cp_vals, decreasing = TRUE),]
 
-  draw.circle(0, 0, df$df_values[1], lwd=line_width)
-  text(0, -0.9, df$df_text[1])
+  plot(0, 0, type = "n", xlim = c(-9, 9), ylim = c(-9, 9), axes=TRUE, asp=1, main=cp_title)
 
-  old <- df$df_values[1]
-  c <- old
+  x_val <- df$cp_vals[1] + df$cp_vals[1] + 1
+  y_val <- 0
+  y_val_text <- - (df$cp_vals[1] + 1)
+
+  draw.circle(0, y_val, df$cp_vals[1], lwd=cp_line_width)
+  text(0, y_val_text, df$cp_text[1])
 
   df <- df[-c(1), ]
 
-  for (x in df$df_values) {
-    c <- c + old + 2
-    draw.circle(c, 0, df$df_values[x], lwd=line_width)
-    text(0, -0.9, df$df_text[x])
+  for (x in df$cp_vals) {
+    draw.circle(x_val, y_val, x, lwd=cp_line_width)
+    text(x_val, y_val_text, x)
+    x_val <- x_val + df$cp_vals[1] + df$cp_vals[1] + 1
   }
 
-
 }
+
+.check_params <- function(cp_vals, cp_text, cp_max, cp_line_width, cp_title) {
+
+  if (class(cp_max) != "integer") {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Parameter 'cp_max' should be integer!")
+  }
+  if (cp_max <= 0L) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Parameter 'cp_max' should be greater than zero!")
+  }
+  if (class(cp_line_width) != "integer") {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Parameter 'cp_line_width' should be integer!")
+  }
+  if (cp_line_width <= 0L) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Parameter 'cp_line_width' should be greater than zero!")
+  }
+  if (is.null(cp_vals) == TRUE) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Vector 'cp_vals' has to be provided!")
+  }
+  if (is.null(cp_text) == TRUE) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Vector 'cp_text' has to be provided!")
+  }
+  if (length(cp_vals) != length(cp_text)) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Both vectors should have same length!")
+  }
+  if (is.numeric(cp_vals) != TRUE) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Vector 'cp_vals' should contain numericals!")
+  }
+  if (is.character(cp_text) != TRUE) {
+    stop("[Error][circlesplot][Error in Parameter(s)]: Vector 'cp_text' should contain characters!")
+  }
+}
+
+
 
