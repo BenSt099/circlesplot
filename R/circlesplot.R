f85f7710af8c4521984ab200dedb2aac9c0d0b9d
BenSt099 <ben.steinhauer.de@gmail.com>
2024-02-03 20:36:57 +0100

    updated plot_circlesplot function

--- R/circlesplot.R
+++ R/circlesplot.R
@@ -1,12 +1,12 @@
 library("plotrix")
 #library("viridis")
 
-colors = c('#D1BBD7', '#AE76A3', '#882E72', '#1965B0', '#5289C7', '#7BAFDE', '#4EB265', '#90C987')
+#colors = c('#D1BBD7', '#AE76A3', '#882E72', '#1965B0', '#5289C7', '#7BAFDE', '#4EB265', '#90C987')
 #circlesplot(cp_vals=c(5,5,4,5,5,5,2,1), cp_text=c('8','7','6','5','4','3','2','1'), cp_max=3L, cp_title="Planets", cp_color=viridis(8))
-circlesplot(cp_vals=c(5,5,4,5,5,5,2,1), cp_text=c('8','7','6','5','4','3','2','1'), cp_max=3L, cp_title="Planets")
+#circlesplot(cp_vals=c(5,5,4,5,5,5,2,1), cp_text=c('8','7','6','5','4','3','2','1'), cp_max=3L, cp_title="Planets")
 
 
-circlesplot <- function(cp_vals=NULL, cp_text=NULL, cp_max=10L, cp_line_width=3L, cp_title="", cp_color=NULL, cp_title_size=1.5) {
+circlesplot <- function(cp_vals=NULL, cp_text=NULL, cp_max=10L, cp_line_width=2L, cp_title="", cp_color=NULL, cp_title_size=1.5) {
 
   .check_params(cp_vals, cp_text, cp_max, cp_line_width, cp_title, cp_color, cp_title_size)
 
@@ -25,12 +25,12 @@ circlesplot <- function(cp_vals=NULL, cp_text=NULL, cp_max=10L, cp_line_width=3L
   diameter <- df$cp_vals[1]
   count <- 0
   x_pos <- 0
-  y_pos <- 0
-  y_pos_text <- -(y_pos + diameter + 3)
+  y_pos <- 5
+  y_pos_text <- y_pos -(diameter + 3)
   color_pos <- 1
 
   par(cex.main = cp_title_size)
-  plot(0, 0, type = "n", xlim = c(-10, 40), ylim = c(-40, 20), axes=TRUE, asp=1, main=cp_title, xlab="", ylab="")
+  plot(0, 0, type = "n", xlim = c(-(diameter *2 + diameter / 2), cp_max * diameter + 2 * diameter), ylim = c(-(diameter* (cp_max*cp_max)), diameter *2 + diameter / 2), axes=TRUE, asp=1, main=cp_title, xlab="", ylab="")
 
   for (item in df$cp_vals) {
 
